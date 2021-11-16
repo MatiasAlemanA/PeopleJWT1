c8c4d0883781553cd2dccde17c25e77e72f4f57d
MatiasAlemanA <a2019114089@estudiantes.upsa.edu.bo>
2021-11-16 00:34:47 -0400

    Instalarslq.server

---
+++ ApiPeopleJWT1/Data/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApiPeopleJWT1.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)// enlaza base de datos
+        {
+
+        }
+    }
+}
